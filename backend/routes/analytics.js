5a827793bfe3d1125f23a90c92dd7ae25fceda32
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-07-11 23:54:07 +0530

    Added geodata-by-link route for fetching user links geodata

--- backend/routes/analytics.js
+++ backend/routes/analytics.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const { getAllLinksAnalytics, getLinkAnalytics } = require('../controllers/analyticsController');
+const { getUserGeodataByLink } = require('../controllers/geodataController');
 const authenticate = require('../middleware/authenticate');
 
 router.get('/', authenticate, getAllLinksAnalytics);
 router.get('/:id', authenticate, getLinkAnalytics);
+router.get('/geodata-by-link', authenticate, getUserGeodataByLink);
 
 module.exports = router; 
