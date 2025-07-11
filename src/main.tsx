ddcf2feef800a91d0e5c7e6acfb18f36369def6d
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-07-11 19:09:20 +0530

    Wrapping of LinksProvider context around app component

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,10 @@
 import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { LinksProvider } from './context/LinksContext';
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <LinksProvider>
+    <App />
+  </LinksProvider>
+);
