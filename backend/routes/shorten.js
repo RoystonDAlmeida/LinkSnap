57e06cd5a4d70c38f495ca7c6827fd0646093b9f
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-07-12 01:19:22 +0530

    Added patch endpoint for enabling/disabling links

--- backend/routes/shorten.js
+++ backend/routes/shorten.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const { shortenUrl } = require('../controllers/shortenController');
+const { updateLinkStatus } = require('../controllers/linkSettingsController');
 const authenticate = require('../middleware/authenticate');
 
 router.post('/', authenticate, shortenUrl);
+router.patch('/:id/status', authenticate, updateLinkStatus);
 
 module.exports = router; 
