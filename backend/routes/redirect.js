aac584f01531b7bb55875d362ce9ae66d379d51a
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-07-13 01:33:43 +0530

    Added POST endpoint while submitting password

--- backend/routes/redirect.js
+++ backend/routes/redirect.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const { handleRedirect } = require('../controllers/redirectController');
 
 router.get('/:id', handleRedirect);
+router.post('/:id', handleRedirect);    // Redirect to URL after submitting password
 
 module.exports = router; 
