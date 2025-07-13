3c62d9652d1da519ad077c1a6e33165eeef653dd
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-07-13 22:22:56 +0530

    Removed link settings endpoints from shorten.js

--- backend/routes/shorten.js
+++ backend/routes/shorten.js
@@ -3,14 +3,10 @@ const router = express.Router();
 
 // Import handlers for the respective routes
 const { shortenUrl } = require('../controllers/shortenController');
-const { updateLinkStatus } = require('../controllers/linkSettingsController');
-const { getQRCode } = require('../controllers/qrController');
 
 // Import authenticate middleware for verifying user
 const authenticate = require('../middleware/authenticate');
 
 router.post('/', authenticate, shortenUrl);
-router.patch('/:id/status', authenticate, updateLinkStatus);
-router.get('/:id/qrcode', authenticate, getQRCode);
 
 module.exports = router; 
